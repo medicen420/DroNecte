1819740598606edfd13cfbf1cf70924c6a3606da
medicen420 <50994647+medicen420@users.noreply.github.com>
2022-08-18 19:23:04 -0500

    ¡LLEGÓ EL DELIVERY!
    Ya he comenzado la programación para que mi Dron pueda soltar las bolsas sobre los clientes que veamos en el transcurso de punto A - B
    
    El jugador deberá soltar las bolsas en el momento correcto, deberá acertar con la posición del cliente al que queramos entregar

---
+++ Assets/Delivery.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    //Esta es una bolsita de weed
+    //public GameObject Bolsita_1;
+
+    public GameObject[] bolsitas;
+    string bolsas_weed = "bolsitas";
+    public int touch_drone;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        bolsitas = GameObject.FindGameObjectsWithTag(bolsas_weed);
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlDelivery();
+
+    }
+
+    public void ControlDelivery()
+    {
+        //Si presionamos la flecha abajo 
+        //ejecutamos el método Deliverweed
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            touch_drone++;
+            if(touch_drone == 1)
+            {
+                Deliverweed();
+            }
+            else if(touch_drone == 2)
+            {
+                Deliverweed_2();
+            }
+        }
+    }
+
+
+
+
+
+    public void Deliverweed()
+    {
+        bolsitas[0].GetComponent<Pruebaa>().enabled = false;
+        bolsitas[0].AddComponent<Rigidbody>();
+        bolsitas[0].GetComponent<Rigidbody>().useGravity = true;
+        //bolsitas[0].AddComponent<Rigidbody>();
+
+        if (touch_drone == 2)
+        {
+            Deliverweed_2();
+        }
+        //-----------------------
+    }
+
+    public void Deliverweed_2()
+    {
+        bolsitas[1].GetComponent<Pruebaa>().enabled = false;
+        bolsitas[1].AddComponent<Rigidbody>();
+        bolsitas[1].GetComponent<Rigidbody>().useGravity = true;
+    }
+
+
+
+
+    //MÉTODO #1
+    /* public void Deliverweed()
+     {
+         //Comentario en consola
+         print("<color=#468C27> Ahí va la mosh madre </color>");
+
+         //Desactivamos el código que nos permite
+         //mantener la bolsa de weed abajo del Dron
+         Bolsita_1.GetComponent<Pruebaa>().enabled = false;
+
+         //Ya que agregaremos el siguiente código
+         //Primero le agregamos a nuestra bolsita de weed un componente del tipo Rigidbody
+         Bolsita_1.AddComponent<Rigidbody>();
+         //Activamos la gravedad para que la bolsa caiga por si sola
+         Bolsita_1.GetComponent<Rigidbody>().useGravity = true;
+
+     }*/
+
+
+}
